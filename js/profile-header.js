7a233c7405c31316dba2688338b6ed36f28159af
luiz007-coder <159715272+luiz007-coder@users.noreply.github.com>
2026-02-16 20:26:29 -0300

    Add files via upload

--- js/profile-header.js
+++ js/profile-header.js
@@ -2,6 +2,7 @@ const ProfileHeader = {
   init() {
     this.render();
     this.attachEvents();
+    this.updateSidebarUser();
   },
 
   getAvatarUrl(nick) {
@@ -23,9 +24,7 @@ const ProfileHeader = {
       <div class="profile-trigger">
         <img src="${avatarUrl}" alt="${user.nick}" class="profile-avatar">
         <span class="profile-nick">${user.nick}</span>
-        <svg width="16" height="16" viewBox="0 0 256 256" fill="currentColor">
-          <path d="M213.66,101.66l-80,80a8,8,0,0,1-11.32,0l-80-80A8,8,0,0,1,53.66,90.34L128,164.69l74.34-74.35a8,8,0,0,1,11.32,11.32Z"></path>
-        </svg>
+        <i class="ph ph-caret-down"></i>
       </div>
       <div class="profile-dropdown">
         <div class="dropdown-user">
@@ -36,25 +35,33 @@ const ProfileHeader = {
           </div>
         </div>
         <div class="dropdown-divider"></div>
-        ${user.isAdmin ? `
-          <a href="painel.html" class="dropdown-item">
-            <svg width="20" height="20" viewBox="0 0 256 256" fill="currentColor">
-              <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm48-88a8,8,0,0,1-8,8H136v32a8,8,0,0,1-16,0V136H88a8,8,0,0,1,0-16h32V88a8,8,0,0,1,16,0v32h32A8,8,0,0,1,176,128Z"></path>
-            </svg>
-            <span>Painel Admin</span>
-          </a>
-        ` : ''}
-        <div class="dropdown-divider"></div>
         <button class="dropdown-item logout-btn" onclick="Auth.logout()">
-          <svg width="20" height="20" viewBox="0 0 256 256" fill="currentColor">
-            <path d="M112,216a8,8,0,0,1-8,8H48a16,16,0,0,1-16-16V48A16,16,0,0,1,48,32h56a8,8,0,0,1,0,16H48V208h56A8,8,0,0,1,112,216Zm109.66-93.66-40-40a8,8,0,0,0-11.32,11.32L196.69,120H104a8,8,0,0,0,0,16h92.69l-26.35,26.34a8,8,0,0,0,11.32,11.32l40-40A8,8,0,0,0,221.66,122.34Z"></path>
-          </svg>
+          <i class="ph ph-sign-out"></i>
           <span>Sair</span>
         </button>
-      </div
       </div>
     `;
     document.body.appendChild(header);
+  },
+
+  updateSidebarUser() {
+    const user = SessionManager.getUser();
+    if (!user) return;
+
+    const avatarUrl = this.getAvatarUrl(user.nick);
+    
+    const sidebarUser = document.querySelector('.sidebar-user-mobile');
+    if (sidebarUser) {
+      sidebarUser.innerHTML = `
+        <div class="user-info">
+          <img src="${avatarUrl}" alt="${user.nick}" class="user-avatar">
+          <div class="user-details">
+            <span class="user-nick">${user.nick}</span>
+            <span class="user-points">${user.points} pontos</span>
+          </div>
+        </div>
+      `;
+    }
   },
 
   attachEvents() {
@@ -75,5 +82,6 @@ const ProfileHeader = {
 
   update() {
     this.render();
+    this.updateSidebarUser();
   }
 };
